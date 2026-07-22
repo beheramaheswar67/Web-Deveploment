6c48df8b07d50f880a6f30dad3456a2b4314e922
beheramaheswar67 <beheramaheswar67@gmail.com>
2026-07-22 20:31:58 +0530

    18.07.2026 to 21.07.2026

---
+++ 04 javascript/lec 1 intro to js, history, keywords, datatypes, variables/diff btn var ,let &  const.js
@@ -0,0 +1,11 @@
+function myFun(){
+    let var1 = 10;
+    const var2 = 20;
+    var var3 = 50;
+}
+
+myFun()
+
+//console.log(var1)
+//console.log(var2)
+console.log(var3)
---
+++ 04 javascript/lec 3  user interaction,operator/userinterface.js
@@ -0,0 +1,17 @@
+// alert("Hello")
+// window.alert("Good Morning")
+
+// let name = prompt("Enter your name")
+// console.log(name)
+// if(name){
+// console.log("Your Name is: "+name)
+// }else{
+//     window.log(prompt("Please re enter your name"))
+// }
+
+let choice = confirm("Are you enjoying js classes ?")
+console.log(choice)
+
+console.log("Hii")
+console.warn("Hello")
+console.error("Bye")
